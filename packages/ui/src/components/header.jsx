5dfe898d6dfab765af70ee4ccdec9fc7283a0cca
Derek Wu <derekwu35@gmail.com>
2024-09-05 15:09:02 -0500

    changed mountain icon to home icon

--- packages/ui/src/components/header.jsx
+++ packages/ui/src/components/header.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import * as React from "react"
-import Link from "next/link"
+import { Home } from "lucide-react"
 import { Badge } from "@repo/ui/components/ui/badge"
 import {
     NavigationMenu,
@@ -17,7 +17,7 @@ export default function NavHeader(props) {
             <div className="flex-1 flex h-32 items-center justify-between p-4">
                 <div className="flex items-center space-x-4">
                     <a href="/" className="flex items-center gap-2">
-                        <MountainIcon className="h-6 w-6 flex items-center" />
+                        <Home className="h-6 w-6 flex items-center" />
                         <span className="text-lg font-semibold">Zeit</span>
                     </a>
                     <NavigationMenu>
@@ -42,24 +42,5 @@ export default function NavHeader(props) {
                 </div>
             </div>
         </header>
-    )
-}
-
-function MountainIcon(props) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-        </svg>
     )
 }
