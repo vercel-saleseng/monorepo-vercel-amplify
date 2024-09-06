06d10b847712966c140c44cf9305ddb812266ca6
Derek Wu <derekwu35@gmail.com>
2024-09-06 15:16:18 -0500

    added footer

--- apps/dashboard/app/page.jsx
+++ apps/dashboard/app/page.jsx
@@ -4,6 +4,7 @@
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
 import Header from "@repo/ui/components/header"
+import Link from "next/link"
 import { Card, CardHeader, CardTitle, CardContent } from "@repo/ui/components/ui/card"
 
 export default function Component() {
@@ -145,6 +146,17 @@ export default function Component() {
           </Card>
         </div>
       </main>
+      <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">© 2024 Zeit. All rights reserved.</p>
+        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+          <Link className="text-xs hover:underline underline-offset-4" href="https://vercel.com">
+            Vercel
+          </Link>
+          <Link className="text-xs hover:underline underline-offset-4" href="https://github.com/vercel-saleseng/monorepo-vercel-amplify">
+            GitHub
+          </Link>
+        </nav>
+      </footer>
     </div>
   )
 }
